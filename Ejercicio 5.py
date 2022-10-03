ca2f288d82cf9c280453b426b1fe7903b22cbde8
Eduardo <ecaballarm@educacion.navarra.es>
2022-10-03 10:18:32 +0200

    Ejercicio 5

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,4 +1,6 @@
-num_horas = float(input("Introduce numero de horas trabajadas\n"))
-coste_hora = float(input("Introduce el coste por hora\n"))
+num_horas = float(input("Introduce numero de horas trabajadas:\n"))
+coste_hora = float(input("Introduce el coste por hora:\n"))
 
-paga = num_horas = coste_hora
+paga = num_horas * coste_hora
+
+print("Tu paga es", paga)
