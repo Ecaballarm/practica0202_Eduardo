a88122696e64e3d5a74ab9d7a009fa96b8442b6d
Eduardo <ecaballarm@educacion.navarra.es>
2022-10-03 11:15:26 +0200

    Ejercicio 8

---
+++ Ejercicio 8.py
@@ -0,0 +1,3 @@
+D = input("Introduce el dividendo:")
+d = input("Introduce el divisor:")
+print(D + " entre " + d + " da un cociente " + str(int(D) // int(d)) + " y un resto " + str(int(D) % int(d)))
